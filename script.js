db891d3c87827dd3304885c297bcc320c5d5ec11
yujiah23 <yuejialow@gmail.com>
2021-05-05 10:49:52 +0800

    updated layout

--- script.js
+++ script.js
@@ -8,14 +8,35 @@ window.onclick = function (event) {
     if (event.target == modal) modal.style.display = "none";
 }
 
+/* if(!localStorage.getItem('year')) {
+    addBookToLibrary();
+} else {
+    displayBook(this, myLibrary);
+} */
+
 function readStatus(e) {
-    var status = document.getElementById('status').checked;
-    if (status == true) {
-        return 'Read';
-    } else {
-        return 'Unread';
+    if(e.type =='checkbox'){
+        var status = document.getElementById('status').checked;
+
+            if (status == true) {
+                return 'Read';
+            } else {
+                return 'Not read';
+            }
+        }
+    if(e.type =='click'){
+    var toggleStatus= document.querySelector(`button[data-num='${e.target.dataset.num}']`);
+        if(toggleStatus.innerHTML=='Read'){
+        toggleStatus.innerHTML='Unread';
+        myLibrary[e.target.dataset.num][status]='unread';
+        } 
+    else if(toggleStatus.innerHTML=='Not read'){
+        toggleStatus.innerHTML='Read';
+        myLibrary[e.target.dataset.num][status]='read';
+    }
     }
 }
+
 function book(title, author, year, pages, status) {
     this.title = title
     this.author = author
@@ -44,23 +65,43 @@ function displayBook() {
     bookList.appendChild(template);
     const templateContent = document.createElement('div');
     templateContent.className = 'templateContent';
-    //template.dataset.num=i;
     template.appendChild(templateContent); 
 
     for (var x in myLibrary[i]) {
         if (Object.prototype.hasOwnProperty.call(myLibrary[i], x)) {
-           
             let displayedTitle=x[0].toUpperCase()+x.slice(1);
-            if (displayedTitle == 'Pages') displayedTitle='No. Of Pages';
-            var item = document.createElement('p');
-            item.className = 'item';
-            item.textContent = ` ${displayedTitle} : ${myLibrary[i][x]}`;
+            if (displayedTitle == 'Title'){
+                var item = document.createElement('h3');
+                item.className = 'item';
+                item.textContent = `${myLibrary[i][x]}`;
+            } else {
+                var item = document.createElement('p');
+                item.className = 'item';
+                    if (displayedTitle == 'Author') {
+                        item.textContent = ` By ${myLibrary[i][x]}`;
+                    }
+                    if (displayedTitle == 'Year') {
+                        item.textContent = ` Year ${myLibrary[i][x]}`;
+                    } 
+                    if (displayedTitle == 'Pages') {
+                        item.textContent = ` ${myLibrary[i][x]} ${displayedTitle} `;
+                    }
+            }
             template.appendChild(item);
+
+            if (displayedTitle == 'Status') {
+                var bookStatusBtn = document.createElement("BUTTON");
+                bookStatusBtn.className = "bookStatusBtn";
+                bookStatusBtn.textContent = `${myLibrary[i][x]}`;
+                template.appendChild(bookStatusBtn);
+                bookStatusBtn.addEventListener('click', readStatus);
+                bookStatusBtn.dataset.num=i;
+            }
         }
     }
     var removeButton = document.createElement("BUTTON");
     removeButton.className = "removeBtn";
-    removeButton.textContent = "REMOVE";
+    removeButton.textContent = "Delete";
     template.appendChild(removeButton);
     removeButton.addEventListener('click', remove);
     removeButton.dataset.num=i;
